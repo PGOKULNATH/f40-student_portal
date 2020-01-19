aa342afa056cfa081a5b4c04a2ddf7d17f53c6e3
S Praveen Kumar <praveens.bec@gmail.com>
2020-01-19 19:08:13 +0530

    Update Courses UI

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,51 +1,80 @@
-import React, {useContext, useEffect} from 'react';
-import Loading from './Loading';
-import Error from './Error';
-import DataContext from '../context/data/dataContext';
-import { Accordion,Button } from "react-bootstrap";
+import React, { useContext, useEffect } from "react";
+import Loading from "./Loading";
+import Error from "./Error";
+import DataContext from "../context/data/dataContext";
+import { Accordion, Button } from "react-bootstrap";
 
 const Courses = () => {
-
   const dataContext = useContext(DataContext);
   const { courses, courses_loading, courses_error, getCourses } = dataContext;
 
   useEffect(() => {
     getCourses();
     //eslint-disable-next-line
-  },[]);
+  }, []);
 
-  if(courses_loading){
-      return <Loading/>
-    }
-
-  else if(courses_error){
+  if (courses_loading) {
+    return <Loading />;
+  } else if (courses_error) {
     console.log(courses_error);
-    return <Error />
+    return <Error />;
   }
 
-  return(
+  return (
     <div className="container">
-      <center><h1>Courses</h1></center>
+      <center>
+        <h1>Courses</h1>
+      </center>
       <div>
         <center>
           <h2>Here are some courses recommended for you</h2>
         </center>
-        <Accordion className= "container">
+        <Accordion className="container">
           {courses.map(item => {
             return (
-              <div className ="row" key={item._id} style={{border:'1px solid #3f99d9',margin:'10px',borderRadius:'5px',boxShadow:'0px 0px 2px 2px #57ebe4'}}>
-                <div className="col-12" style={{padding:'0px',margin:'0px'}}>
-                  <Accordion.Toggle as={Button} eventKey={item._id} className="col-12" style={{fontSize : '20px', padding:'15px'}}>
-                    <p style={{float : 'left'}}>{item.title}</p>
-                    <p className="badge badge-success" style={{float : 'right', fontSize : '20px', padding : '10px 20px', backgroundColor : '#34e02b'}}>{item.price}</p>
+              <div className="row m-2 shadow" key={item._id}>
+                <div className="col-12 m-0 p-0">
+                  <Accordion.Toggle
+                    as={Button}
+                    eventKey={item._id}
+                    className="col-12 p-2 font-big m-neutral-bg"
+                  >
+                    <p className="float-left font-big">{item.title}</p>
+                    <p className="badge badge-success float-right p-3">
+                      {item.price}
+                    </p>
                   </Accordion.Toggle>
                 </div>
-                <Accordion.Collapse eventKey={item._id} className="col-12" style={{padding:'10px',margin:'5px'}}>
-                  <div className="col-12">
-                    <p style={{textAlign : 'justify', padding:'2px', fontWeight:'bold', fontSize:'25px'}}>Description : </p>
-                    <p>{item.desc}</p>
-                    <p style={{padding:'2px', fontWeight:'bold', fontSize:'25px'}}>Financial Aid available :  {item.financialaid === 'Yes' ? <span style={{color : 'green'}}>Yes</span> : (item.financialaid === 'No' ? <span style={{color : 'red'}}>No</span> : <span style={{color : '#deed05'}}>Not Required</span> ) } </p>
-                    <a href={item.link} target="_blank" rel="noopener noreferrer" style={{backgroundColor : '#99087c', color:'white', fontSize : '20px'}} className="btn btn-block">Go to Course</a>
+                <Accordion.Collapse eventKey={item._id} className="col-12">
+                  <div className="row p-2">
+                    <div className="col-12">
+                      <h5>
+                        <b>Description : </b>
+                      </h5>
+                      <p>{item.desc}</p>
+                      <h5>
+                        <b>
+                          Financial Aid available :{" "}
+                          {item.financialaid === "Yes" ? (
+                            <span className="text-success">Yes</span>
+                          ) : item.financialaid === "No" ? (
+                            <span className="text-danger">No</span>
+                          ) : (
+                            <span className="text-warning">Not Required</span>
+                          )}{" "}
+                        </b>
+                      </h5>
+                    </div>
+                    <div className="col-8 offset-2 col-md-4 offset-md-4">
+                      <a
+                        href={item.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn btn-block m-primary-bg text-white"
+                      >
+                        Go to Course
+                      </a>
+                    </div>
                   </div>
                 </Accordion.Collapse>
               </div>
